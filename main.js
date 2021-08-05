e6cd55c93c8d9a6bea63de29fda2e7de294cc7c2
Jairo2544 <76968343+Jairo2544@users.noreply.github.com>
2021-08-05 18:42:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 document.querySelector(".boton-res").addEventListener("click", () => {
 	document.querySelector(".nav-ul").classList.toggle("show")
 });
+
+var recommendNumber = 0
+
+document.querySelector(".right").addEventListener("click", () => {
+	var recommend = ".recommend" + recommendNumber;
+	document.querySelector(recommend).classList.toggle("active");
+	console.log(recommend);
+	if (recommendNumber >= 2) {
+		recommendNumber = 0
+	}
+	else {
+		recommendNumber = recommendNumber + 1
+	};
+	var recommend = ".recommend" + recommendNumber;
+	console.log(recommend);
+	document.querySelector(recommend).classList.toggle("active");
+});
