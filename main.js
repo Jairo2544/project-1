116a7cd67f956a748877696dfffaf562980aa9e7
Jairo2544 <76968343+Jairo2544@users.noreply.github.com>
2021-08-05 20:19:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,17 +4,53 @@ document.querySelector(".boton-res").addEventListener("click", () => {
 
 var recommendNumber = 0
 
-document.querySelector(".right").addEventListener("click", () => {
-	var recommend = ".recommend" + recommendNumber;
-	document.querySelector(recommend).classList.toggle("active");
-	console.log(recommend);
-	if (recommendNumber >= 2) {
-		recommendNumber = 0
-	}
-	else {
-		recommendNumber = recommendNumber + 1
-	};
-	var recommend = ".recommend" + recommendNumber;
-	console.log(recommend);
-	document.querySelector(recommend).classList.toggle("active");
-});
+/*Recommend-Flecha derecha*/
+	document.querySelector(".right").addEventListener("click", () => {
+		/*texto*/
+			var recommend = ".recommend" + recommendNumber;
+			var rcoloro = "r-color" + recommendNumber;
+			var backo = "back" + recommendNumber;
+
+			document.querySelector(recommend).classList.toggle("active");
+			if (recommendNumber >= 2) {
+				recommendNumber = 0
+			}
+			else {
+				recommendNumber = recommendNumber + 1
+			};
+			var recommend = ".recommend" + recommendNumber;
+			document.querySelector(recommend).classList.toggle("active");
+
+		/*Background*/
+			var backn = "back" + recommendNumber;
+			document.querySelector(".container-notice").classList.replace(backo, backn);
+
+		/*color*/
+			var rcolorn = "r-color" + recommendNumber;
+			document.querySelector(".container-tittle").classList.replace(rcoloro, rcolorn);
+	});
+
+	/*Recommend-Flecha izquierda*/
+	document.querySelector(".left").addEventListener("click", () => {
+		/*texto*/
+			var recommend = ".recommend" + recommendNumber;
+			document.querySelector(recommend).classList.toggle("active");
+			if (recommendNumber <= 0) {
+				recommendNumber = 2
+			}
+			else {
+				recommendNumber = recommendNumber - 1
+			};
+			var recommend = ".recommend" + recommendNumber;
+			document.querySelector(recommend).classList.toggle("active");
+
+		/*Background*/
+			var back = "back" + recommendNumber;
+			document.querySelector(".container-notice").classList.remove("back0", "back1", "back2");
+			document.querySelector(".container-notice").classList.add(back);
+
+		/*color*/
+			var rcolor = "r-color" + recommendNumber;
+			document.querySelector(".container-tittle").classList.remove("r-color0", "r-color1", "r-color2");
+			document.querySelector(".container-tittle").classList.add(rcolor);
+	});
