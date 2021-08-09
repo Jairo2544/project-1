a05a38a92f9339b632b7c08268ae323674f8a1fc
Jairo2544 <76968343+Jairo2544@users.noreply.github.com>
2021-08-09 10:32:47 -0500

    Update nav-menu-generator.js

--- javaScript/nav-menu-generator.js
+++ javaScript/nav-menu-generator.js
@@ -15,7 +15,7 @@
 		</ul>
 	</div>
 */
-    var navMenu ='<div class="nav-container">    <a href="index.html"><img class="nav-img" src="img/logo.png" alt="logo"></a>    <div class="boton-res">        <i class="fas fa-bars"></i>    </div>    <ul class="nav-ul">        <li class="nav-ul-li"><a href="index.html#inicio">INICIO</a></li>        <li class="nav-ul-li"><a href="index.html#animes">ANIMES</a></li>        <li class="nav-ul-li"><a href="index.html#productos">PRODUCTOS</a></li>        <li class="nav-ul-li"><a href="index.html#redes">REDES</a></li>	        <li class="nav-ul-li"><a href="foro.html">FORO</a></li>	    </ul>   </div>'
+    var navMenu ='<div class="nav-container">    <a href="index.html"><img class="nav-img" src="/project1/img/logo.png" alt="logo"></a>    <div class="boton-res">        <i class="fas fa-bars"></i>    </div>    <ul class="nav-ul">        <li class="nav-ul-li"><a href="index.html#inicio">INICIO</a></li>        <li class="nav-ul-li"><a href="index.html#animes">ANIMES</a></li>        <li class="nav-ul-li"><a href="index.html#productos">PRODUCTOS</a></li>        <li class="nav-ul-li"><a href="index.html#redes">REDES</a></li>	        <li class="nav-ul-li"><a href="foro.html">FORO</a></li>	    </ul>   </div>'
 
     document.write(navMenu)
 
